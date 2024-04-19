3290ced26ff53e06995e81410bd8f0dee3b8dc5d
Jyanon99 <71812148+Jyanon99@users.noreply.github.com>
2024-04-19 12:30:20 +0800

    can_play_version_1
    -add into, opening, real end, true end scenes
    -build exe
    you can try to play it now

---
+++ Assets/Scripts/VideoChanger.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VideoChanger : MonoBehaviour
+{
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            SceneManager.LoadScene("game");
+        }
+    }
+
+}
