c07cba870e9e7c1644ff58bb6d8d64e0f5eca42e
Jyanon99 <71812148+Jyanon99@users.noreply.github.com>
2024-04-26 12:55:22 +0800

    create info page (not finish)

--- Assets/Scripts/IntroUI.cs
+++ Assets/Scripts/IntroUI.cs
@@ -5,18 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class IntroUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void StartGame()
     {
         SceneManager.LoadScene("opening");
@@ -25,5 +13,15 @@ public class IntroUI : MonoBehaviour
     public void ExitGame()
     {
         Application.Quit();
+    }
+
+    public void GoInfo()
+    {
+        SceneManager.LoadScene("info");
+    }
+
+    public void BackIntro()
+    {
+        SceneManager.LoadScene("intro");
     }
 }
